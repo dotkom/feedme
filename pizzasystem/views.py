88f80c2e70c316b30dd4e753b2cae8cff8dcf32e
Tor Håkon Bonsaksen <torrib90@gmail.com>
2013-01-30 20:02:45 +0100

    back to render_to_response

--- pizzasystem/views.py
+++ pizzasystem/views.py
@@ -1,14 +1,14 @@
 from django.template import Template, Context, loader, RequestContext
-from django.shortcuts import render
+from django.shortcuts import render, render_to_response
 from django.http import HttpResponse
 from pizzasystem.models import PizzaSystem
 
 def index(request):
     orders = PizzaSystem.objects.all()
-    return render('pizzasystem/index.html', {'orders' : orders})
+    return render_to_response('pizzasystem/index.html', {'orders' : orders})
 
 def newpizza(request):
     user = request.user
     pizza = PizzaSystem()
-    return render('pizzasystem/newpizza.html', {'pizza' : pizza, 'user' : user})
+    return render_to_response('pizzasystem/newpizza.html', {'pizza' : pizza, 'user' : user})
     
