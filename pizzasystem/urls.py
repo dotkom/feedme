5cb0ee4ee29b2b7469dfa6b3bd4358af7b8f34c9
Tor Håkon Bonsaksen <torrib90@gmail.com>
2013-01-30 16:47:36 +0100

    Started on newpizza functionality

--- pizzasystem/urls.py
+++ pizzasystem/urls.py
@@ -2,4 +2,5 @@ from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('pizzasystem.views',
     url(r'^$', 'index', name='pizza_index'),
+    url(r'^newpizza', 'newpizza', name='new_pizza'),
 )
