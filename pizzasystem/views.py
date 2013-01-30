63ca68dd5d313ccea44ed46638a1e2df490f75a6
Tor Håkon Bonsaksen <torrib90@gmail.com>
2013-01-30 23:09:14 +0100

    Started on POST (not working yet)

--- pizzasystem/views.py
+++ pizzasystem/views.py
@@ -1,5 +1,5 @@
 from django.template import Template, Context, loader, RequestContext
-from django.shortcuts import render, render_to_response
+from django.shortcuts import render
 from django.http import HttpResponse
 from pizzasystem.models import Order, Pizza
 
@@ -9,6 +9,11 @@ def index(request):
 
 def newpizza(request):
     default_pizza = Pizza()
-    order = Order.objects.all().latest()    
+    order = Order.objects.all().latest()
+    default_pizza.order = order    
     return render(request, 'pizzasystem/newpizza.html', {'pizza' : default_pizza, 'order' : order})
+
+def add(request, command):
+    #p = get_object_or_404(Pizza)
+    return render(request, 'Bestilt(NOT)')
     
