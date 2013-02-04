9d0e8a6d4f40c53f704210273667e79f2c070075
Tor Håkon Bonsaksen <torrib90@gmail.com>
2013-02-04 23:17:05 +0100

    Commit before i remake som funcions

--- pizzasystem/urls.py
+++ pizzasystem/urls.py
@@ -4,4 +4,6 @@ urlpatterns = patterns('pizzasystem.views',
     url(r'^$', 'index', name='pizza_index'),
     url(r'^newpizza', 'newpizza', name='new_pizza'),
     url(r'^add', 'add', name='add_pizza'),
+    url(r'^(?P<pizza_id>\d+)/$','edit', name='edit'),
+    url(r'^edit', 'edit_function', name='edit_function'),
 )
--- pizzasystem/views.py
+++ pizzasystem/views.py
@@ -1,5 +1,5 @@
 from django.template import Template, Context, loader, RequestContext
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from pizzasystem.models import Order, Pizza
 from forms import PizzaForm
@@ -17,19 +17,32 @@ def newpizza(request):
         return render(request, 'pizzasystem/newpizza.html', {'form' : form})
     return HttpResponse('Nope')
 
-def add(request):
+def save(request):
     if is_allowed(request):
         form = PizzaForm(request.POST)
         if form.is_valid():
-            order = Order.objects.all().latest()
             pizza = form.save(commit=False)
             pizza.user = request.user
-            pizza.order = order
-            form.save()
-            return render(request, 'pizzasystem/index.html', {'order' : order})
+            pizza.order = Order.objects.all().latest()
+            pizza.save()
+            return index(request)
         return HttpResponse('Not a valid form')
     return HttpResponse('Nope')
     
+def edit(request, pizza_id):
+    if is_allowed(request):
+        pizza = get_object_or_404(Pizza, pk=pizza_id)
+        form = PizzaForm(instance=pizza)
+        return render(request, 'pizzasystem/editpizza.html', {'form' : form})
+    return HttpResponse('Nope')
+
+def edit_function(request):
+    pizza = PizzaForm(request.POST).save(commit=False)
+    pizza.save()
+    return index(request)
+        
+        
+
 
 def is_allowed(request):
     allowed = Order.objects.all().latest().pizza_users()
