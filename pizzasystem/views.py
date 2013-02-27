866e95ea92d23b3e235984b2bc0c5872ee098c3d
Tor Håkon Bonsaksen <torrib90@gmail.com>
2013-02-27 20:18:25 +0100

    Added group checks for pizza ordering and admining

--- pizzasystem/views.py
+++ pizzasystem/views.py
@@ -3,37 +3,40 @@ from django.shortcuts import render, get_object_or_404, redirect
 from django.http import HttpResponse, HttpResponseRedirect
 from pizzasystem.models import Order, Pizza, Admin, Saldo, OrderLimit
 from forms import PizzaForm, AdminForm, OrderLimitForm
+from django.contrib.auth.decorators import login_required, user_passes_test
 
+@login_required
+@user_passes_test(lambda u: u.groups.filter(name='pizza').count() == 1)
 def index(request):
-    if(is_allowed(request)):
-        order = Order.objects.all().latest()
-        return render(request, 'index.html', {'order' : order})
-    return denied()
+    order = Order.objects.all().latest()
+    return render(request, 'index.html', {'order' : order})
 
+@login_required
+@user_passes_test(lambda u: u.groups.filter(name='pizza').count() == 1)
 def pizzaview(request, pizza_id=None):
-    if is_allowed(request):
-        if pizza_id == None:
-            pizza = Pizza()
-        else:
-            pizza = get_object_or_404(Pizza, pk=pizza_id)
+    if pizza_id == None:
+        pizza = Pizza()
+    else:
+        pizza = get_object_or_404(Pizza, pk=pizza_id)
 
-        if request.method == 'POST':
-            form = PizzaForm(request.POST, instance=pizza)
-            if form.is_valid():
-                form = form.save(commit=False)
-                form.user = request.user
-                return validate_users_saldo(request, form)
-            else:
-                return HttpResponse('Invalid input')
+    if request.method == 'POST':
+        form = PizzaForm(request.POST, instance=pizza)
+        if form.is_valid():
+            form = form.save(commit=False)
+            form.user = request.user
+            return validate_users_saldo(request, form)
         else:
-            form = PizzaForm(instance=pizza)
-            if pizza_id:
-                form.fields["buddy"].queryset = Order.objects.all().latest().free_users(pizza.buddy, pizza.user)
-            else: 
-                form.fields["buddy"].queryset = Order.objects.all().latest().free_users()
-            return render(request, 'pizzaview.html', {'form' : form})
-    return denied()
+            return HttpResponse('Invalid input')
+    else:
+        form = PizzaForm(instance=pizza)
+        if pizza_id:
+            form.fields["buddy"].queryset = Order.objects.all().latest().free_users(pizza.buddy, pizza.user)
+        else: 
+            form.fields["buddy"].queryset = Order.objects.all().latest().free_users()
+        return render(request, 'pizzaview.html', {'form' : form})
 
+@login_required
+@user_passes_test(lambda u: u.groups.filter(name='pizzaadmin').count() == 1)
 def admin(request):
     order_limit = get_order_limit()
 
@@ -58,15 +61,17 @@ def admin(request):
         order_limit_form = OrderLimitForm(instance=order_limit)
         return render(request, 'admin.html', {'form' : form, 'order_limit_form' : order_limit_form})
 
+@login_required
+@user_passes_test(lambda u: u.groups.filter(name='pizza').count() == 1)
 def edit(request, pizza_id):
     return pizzaview(request, pizza_id)
 
+@login_required
+@user_passes_test(lambda u: u.groups.filter(name='pizza').count() == 1)
 def delete(request, pizza_id):
-    if is_allowed(request):
-        pizza = get_object_or_404(Pizza, pk=pizza_id)
-        pizza.delete()
-        return index(request)
-    return denied()
+    pizza = get_object_or_404(Pizza, pk=pizza_id)
+    pizza.delete()
+    return index(request)
 
 def get_order_limit():
     order_limit = OrderLimit.objects.all()
@@ -93,6 +98,7 @@ def validate_users_saldo(request, form):
     form.save()
     return index(request)   
 
+#Depricated
 def is_allowed(request):
     allowed = Order.objects.all().latest().pizza_users()
     for user in allowed:
@@ -100,6 +106,7 @@ def is_allowed(request):
             return True
     return False
 
+#depricated
 def denied():
     return HttpResponse('Permission denied')
 
@@ -115,13 +122,13 @@ def handle_payment(data):
         #order.save()
     else:
         print "Ikke noe order valgt"
-    
+
 def handle_deposit(data):
     users = data['users']
     deposit = data['add_value']
-    
+
     handle_saldo(users, deposit) 
-        
+    
 def handle_saldo(users, value):
     for user in users:
         saldo = user.saldo_set.get()
@@ -137,4 +144,4 @@ def validate_or_create_saldo():
             saldo.user = user
             saldo.save()
         
-    
+
