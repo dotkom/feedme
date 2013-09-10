8567adbb4311b35369752a38a12168f134c305c2
Håkon <torrib90@gmail.com>
2013-09-10 18:37:09 +0200

    Added login url"

--- pizza/settings.py
+++ pizza/settings.py
@@ -16,6 +16,8 @@ ADMINS = (
 
 MANAGERS = ADMINS
 
+LOGIN_URL = "/admin/"
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
