ef3ef078d47cd935131571fd42d5d76788e81ae0
Christian Strand Young <christian@strandyoung.com>
2013-10-25 00:46:14 +0200

    Fixing user shit

--- pizzasystem/models.py
+++ pizzasystem/models.py
@@ -4,9 +4,9 @@ from django.db import models
 from django.contrib.auth.models import Group
 from django.utils.translation import ugettext_lazy as _
 from django.conf import settings
-from django.contrib.auth import get_user_model
+#from django.contrib.auth import get_user_model
 
-User = get_user_model()
+User = getattr(settings, 'AUTH_USER_MODEL', 'auth.User') 
 
 class OrderLine(models.Model):
     date = models.DateField(_("dato"))
