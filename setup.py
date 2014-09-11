0473fe93d2935a5352763b03daff747bd34fb262
Sklirg <hlsolbjorg@gmail.com>
2014-09-11 03:09:45 +0200

    added setup.py

---
+++ setup.py
@@ -0,0 +1,32 @@
+import os
+from setuptools import setup
+
+README = open(os.path.join(os.path.dirname(__file__), 'README.md')).read()
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+setup(
+    name='feedme',
+    version='1.0.0',
+    packages=['feedme'],
+    include_package_data=True,
+    license='BSD License',  # example license
+    description='Food ordering management for onlineweb4',
+    long_description=README,
+    url='http://online.ntnu.no/feedme',
+    author='dotkom',
+    author_email='dotkom@online.ntnu.no',
+    classifiers=[
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License', # example license
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+    ],
+)
