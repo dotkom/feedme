f39041503e389355fe30796be1c04ff70be2b964
Sklirg <hlsolbjorg@gmail.com>
2014-09-17 19:58:18 +0200

    bump for release

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='feedme',
-    version='1.0.2',
+    version='1.0.3',
     packages=['feedme'],
     include_package_data=True,
     license='BSD License',  # example license
