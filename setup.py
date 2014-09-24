230cf4ce271a4aaed37c35692ac9df526c20de9f
Håkon Solbjørg <hlsolbjorg@gmail.com>
2014-09-24 19:17:19 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='feedme',
-    version='1.0.10',
+    version='1.0.11',
     author='dotkom',
     author_email='dotkom@online.ntnu.no',
     #packages=['feedme'],
