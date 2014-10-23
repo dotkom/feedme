d30f99511faea6b0fd5305c9907ae74cd9e56669
sklirg <hlsolbjorg@gmail.com>
2014-10-23 02:20:29 +0200

    Fixed typo in coverage import

--- runtests.py
+++ runtests.py
@@ -40,7 +40,7 @@ def run_tests(*test_args):
     if not test_args:
         try:
             # This runs on Online/dotKom Jenkins to create statistics
-            import nosexcov
+            import nosexcover
             test_args = ['feedme', '--with-xunit', '--with-xcoverage', '--cover-package=feedme']
         except:
             test_args = ['feedme', '--with-coverage','--cover-package=feedme']
