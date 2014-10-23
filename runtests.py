46fc7150b51ff3ec78221f4d0ef86adb10eb9713
sklirg <hlsolbjorg@gmail.com>
2014-10-23 02:14:56 +0200

    Try to run tests on Jenkins with coverage

--- runtests.py
+++ runtests.py
@@ -38,9 +38,13 @@ except ImportError:
 
 def run_tests(*test_args):
     if not test_args:
-        # Jenkins can create pretty stats with this
-        #test_args = ['feedme', '--with-xunit', '--with-xcoverage', '--cover-package=feedme']
-        test_args = ['feedme', '--with-coverage','--cover-package=feedme']
+        try:
+            # This runs on Online/dotKom Jenkins to create statistics
+            import nosexcov
+            test_args = ['feedme', '--with-xunit', '--with-xcoverage', '--cover-package=feedme']
+        except:
+            test_args = ['feedme', '--with-coverage','--cover-package=feedme']
+
 
     # Run tests
     test_runner = NoseTestSuiteRunner(verbosity=1)
