dc6759920ca2d7ae96540816e2cdceb64f8cf6b6
sklirg <hlsolbjorg@gmail.com>
2015-02-18 23:53:23 +0100

    Release 1.6.10

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='feedme',
-    version='1.6.9',
+    version='1.6.10',
     author='dotkom',
     author_email='dotkom@online.ntnu.no',
     packages=['feedme'],
