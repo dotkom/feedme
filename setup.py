4dac94534b5c6f7dd799a45ae9823554846cc297
sklirg <hlsolbjorg@gmail.com>
2015-04-14 11:20:46 +0200

    Release 1.7.3

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='feedme',
-    version='1.7.2',
+    version='1.7.3',
     author='dotkom',
     author_email='dotkom@online.ntnu.no',
     packages=['feedme'],
