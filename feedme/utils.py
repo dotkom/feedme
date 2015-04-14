0614289475c93ffc3f0e47ae5f469940ae40b59d
sklirg <hlsolbjorg@gmail.com>
2015-04-14 11:39:21 +0200

    Change how we get groups, allowing people to set groups themselves

--- feedme/utils.py
+++ feedme/utils.py
@@ -1,5 +1,7 @@
 from django.contrib.auth.models import Group
+from django.conf import settings
 
 def get_feedme_groups():
-    groups = Group.objects.filter(name__icontains='kom')
+    group_ids = settings.FEEDME_GROUPS
+    groups = [Group.objects.get(pk=i) for i in group_ids]
     return groups
