1af982cb4dbf1bb9824bf8b2c219f2fba855d5e9
sklirg <hlsolbjorg@gmail.com>
2015-04-28 18:14:36 +0200

    Remove example settings file

--- feedme/example-feedme.py
+++
@@ -1,9 +0,0 @@
-# This is an example feedme-settings file. Include these settings in your base settings.
-
-
-# A list of Group IDs that should be allowed to use feedme.
-# Every group will have their own section
-FEEDME_GROUPS = []
-
-# An admin group with access to the admin control panel for feedme.
-FEEDME_ADMIN_GROUP = 'feedmeadmin'
