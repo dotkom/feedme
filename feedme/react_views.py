81c0d7063dccc51acea904efbe798f9b501efb4f
sklirg <hlsolbjorg@gmail.com>
2015-11-02 01:15:25 +0100

    Prepare for some group selection

--- feedme/react_views.py
+++ feedme/react_views.py
@@ -5,6 +5,7 @@ from feedme.views import get_order
 
 def react_index(request):
     context = {}
+    context['groups'] = request.user.groups.all()
     context['order'] = get_order(request.user.groups.all()[0])
     context['username'] = request.user
 
